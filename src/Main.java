f28a384f57ca72a309601156a3d8fbea5b20ac0b
akylai <nurbekovaakylaj10@gmail.com>
2026-01-28 17:08:46 +0600

    oop sozdala pro person

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+       house person1=new house();
+       person1.fio=" Nurbekova Akylai";
+       person1.age=20;
+       person1.gender="j";
+       person1.nation="kyrgyz";
+       person1.holostoi=true;
+
+
+        System.out.println("fio:"+person1.fio);
+        System.out.println("age:"+person1.age);
+        System.out.println("gender:"+person1.gender);
+        System.out.println("nation:"+person1.nation);
+        System.out.println("holostoi:"+person1.holostoi);
+
+        System.out.println(person1.run());
+        System.out.println(person1.speak());
+        System.out.println(person1.getBirthDate());
+
+
+
+
+
+
+    }
+}
---
+++ src/house.java
@@ -0,0 +1,20 @@
+public class house {
+      String fio;
+      int age;
+      String nation;
+      String gender;
+      String profession;
+      boolean holostoi;
+
+      public  String run(){
+          return  this.fio+" mojet begat";
+      }
+  public  String speak(){
+          return  this.fio+" mojet rasgovarivate";
+  }
+ public  int getBirthDate(){
+          return  2026-this.age;
+ }
+
+}
+
