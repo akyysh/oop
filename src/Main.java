f7c41e1e05bf7c1fd9612c899f1b32363c8a9831
akylai <nurbekovaakylaj10@gmail.com>
2026-01-28 18:37:26 +0600

    oop programmer

--- src/Main.java
+++ src/Main.java
@@ -1,84 +1,33 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        ynivers kolledj=new ynivers();
-        kolledj.name="BMK";
-        kolledj.age=85;
-        kolledj.fakyltet=6;
-        kolledj.yroki=35;
-        kolledj.ayditorii=80;
-        kolledj.students=15000;
-        kolledj.ychiteli=100;
-        kolledj.korpus=4;
-        kolledj.fudKord=1;
-        kolledj.filials=1;
-
-
-        System.out.println("name"+kolledj.name);
-       System.out.println("age:"+kolledj.age);
-        System.out.println("fakyltet:"+kolledj.fakyltet);
-        System.out.println("yroki:"+kolledj.yroki);
-        System.out.println("ayditorii:"+kolledj.ayditorii);
-        System.out.println("students:"+kolledj.students);
-       System.out.println(" ychiteli:"+kolledj.ychiteli);
-        System.out.println(" korpus:"+kolledj.korpus);
-        System.out.println("fud cord:"+kolledj.fudKord);
-        System.out.println("filialsi:"+kolledj.filials);
-
-
-        ynivers kgma=new ynivers();
-        kgma.name="kgma";
-        kgma.age=100;
-        kgma.fakyltet=10;
-        kgma.yroki=40;
-        kgma.ayditorii=300;
-        kgma.students=50000;
-        kgma.ychiteli=200;
-        kgma.korpus=5;
-        kgma.fudKord=3;
-        kgma.filials=3;
-
-        System.out.println("name"+kgma.name);
-        System.out.println("age:"+kgma.age);
-        System.out.println("fakyltet:"+kgma.fakyltet);
-        System.out.println("yroki:"+kgma.yroki);
-        System.out.println("ayditorii:"+kgma.ayditorii);
-        System.out.println("students:"+kgma.students);
-        System.out.println(" ychiteli:"+kgma.ychiteli);
-        System.out.println(" korpus:"+kgma.korpus);
-        System.out.println("fud cord:"+kgma.fudKord);
-        System.out.println("filialsi:"+kgma.filials);
-
-
-        ynivers arabaeva=new ynivers();
-        arabaeva.name="ARABAEVA";
-        arabaeva.age=104;
-        arabaeva.fakyltet=30;
-        arabaeva.yroki=50;
-        arabaeva.ayditorii=300;
-        arabaeva.students=16000;
-        arabaeva.ychiteli=140;
-        arabaeva.korpus=5;
-        arabaeva.fudKord=1;
-        arabaeva.filials=2;
-
-        System.out.println("name"+arabaeva.name);
-        System.out.println("age:"+arabaeva.age);
-        System.out.println("fakyltet:"+arabaeva.fakyltet);
-        System.out.println("yroki:"+arabaeva.yroki);
-        System.out.println("ayditorii:"+arabaeva.ayditorii);
-        System.out.println("students:"+arabaeva.students);
-        System.out.println(" ychiteli:"+arabaeva.ychiteli);
-        System.out.println(" korpus:"+arabaeva.korpus);
-        System.out.println("fud cord:"+arabaeva.fudKord);
-        System.out.println("filialsi:"+arabaeva.filials);
 
 
 
+  programmer  sister=new programmer();
+  Scanner scanner=new Scanner(System.in);
+    sister.name= scanner.next();
+    sister.surname= scanner.next();
+    sister.age= scanner.nextInt();
+
+        System.out.println("NAME:"+sister.name);
+        System.out.println("SURNAME:"+sister.surname);
+        System.out.println("AGE:"+sister.age);
+
+        programmer   bro=new programmer();
+        bro.name= scanner.next();
+        bro.surname= scanner.next();
+        bro.age= scanner.nextInt();
+
+
+        System.out.println("NAME:"+bro.name);
+        System.out.println("SURNAME:"+bro.surname);
+        System.out.println("AGE:"+bro.age);
 
 
 
-
-   }
+    }
 }
---
+++ src/programmer.java
@@ -0,0 +1,9 @@
+public class programmer {
+    String name;
+    String surname;
+    int age;
+
+    public int birthdayDate(){
+        return  2026-this.age;
+    }
+}
